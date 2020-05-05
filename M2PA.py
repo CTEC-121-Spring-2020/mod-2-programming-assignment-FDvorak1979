fe0cee418df1a8b6917fd38233a39f791e4adbb4
FDvorak1979 <f.dvorak2@students.clark.edu>
2020-05-05 16:39:47 -0700

    demonstaration of math

--- M2PA.py
+++ M2PA.py
@@ -37,15 +37,37 @@ def main():
     print("one", "two", "three")
     print("\tdemo backslash escape character: path=c:\\folder\\folder\\filename")
     print()
-    '''
+    ze = 4968054798 / 0
+    print(ze)
+ZeroDivisionError: division by zero
+'''
     print("test")
 
+    a = math.sqrt(387)
     y = 456
     r = 212
     x = 4 * math.pi / math.cosh(y) + r**3
     z = x + y
+    z1 = x + y / math.ceil(a)
+    b = a + z * math.floor(4587.33336)
+    v = 4 / 3 * math.pi**3
+    e = 4 * math.pi * r**2
+    f = math.sin(r) * math.pi**3
+
+ 
 
     print()
+    print(a)
     print(x)
+    print(z1)
+    print(b)
+    print(v)
+    print(y**2)
+    print(r**3)
+    print(a+y+r+z+z1+b+v+e)
+    print(f+e)
+    print(f+e / math.tan(z))
+main()
 
-main()   
+
+   
